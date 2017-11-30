efca1582d804d981f10da2acba1b7481d56ce607
Evan Schultz <evan@rangle.io>
2017-11-29 19:20:55 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { NgReduxRouter } from './router';
 import { RouterAction, routerReducer } from './reducer';
 import { UPDATE_LOCATION } from './actions';
 
 @NgModule({
-  providers: [ NgReduxRouter ]
+  // providers: [ NgReduxRouter ]
 })
-export class NgReduxRouterModule { }
+export class NgReduxRouterModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgReduxRouterModule,
+      providers: [NgReduxRouter]
+    };
+  }
+}
 
-export {
-  NgReduxRouter,
-  RouterAction,
-  routerReducer,
-  UPDATE_LOCATION
-};
+export { NgReduxRouter, RouterAction, routerReducer, UPDATE_LOCATION };
